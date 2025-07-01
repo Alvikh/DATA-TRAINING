362e5ff2891b7c9dae2c3e192ec4d806b8201e6c
Alvikh <khoirunnisaalvi4@gmail.com>
2025-07-01 22:51:34 +0700

    db config

--- app.py
+++ app.py
@@ -238,4 +238,4 @@ if __name__ == "__main__":
     except Exception as e:
         logger.error(f"Failed to initialize MQTT: {e}")
     
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5050)))
